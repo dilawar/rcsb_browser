2760f225b23558e8e6b22efae8e617cda8508e0c
Dilawar <dilawars@iitb.ac.in>
2014-08-29 16:26:42 +0530

    Added __init__.py files to directories.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md") as f:
+    readme = f.read()
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    ]
+
+setup(
+    name = "rcsb_browser",
+    version = "0.0.1",
+    description = "Short description of package",
+    long_description = readme,
+    packages = ["browser", "downloader" ],
+    package_data = {},
+    install_requires = [ ],
+    author = "Dilawar Singh",
+    author_email = "dilawars@ncbs.res.in",
+    url = "http://github.com/dilawar/",
+    license='GPL',
+    classifiers=classifiers,
+)
