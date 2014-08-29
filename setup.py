31186c733056b93663f49684bbca8fa4b41e4872
Dilawar <dilawars@iitb.ac.in>
2014-08-29 18:27:47 +0530

    Report generation is working, more of less.

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+
+"""setup.py: Setup file.
+
+Last modified: Sat Jan 18, 2014  05:01PM
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2013, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
 import os
 import sys
 
@@ -5,6 +21,7 @@ from setuptools import setup
 
 with open("README.md") as f:
     readme = f.read()
+
 classifiers = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
